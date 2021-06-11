65e9cff9ef37c7c4fc141a63a19f60233f46308b
Tiru <tiru@live.in>
2021-06-11 19:25:01 +0530

    modified html

---
+++ vueApp.js
@@ -0,0 +1,13 @@
+const url = "/api/getdevices";
+
+const vm = new Vue({
+        el: '#app',
+        data: {
+          results: []
+        },
+        mounted() {
+          axios.get(url).then(response => {
+            this.results = response.data
+          })
+        }
+      });
