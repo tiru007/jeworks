6809899b25b1158cc97a567bf8951a7f698c009a
Tiru <tiru@live.in>
2021-06-12 18:08:54 +0530

    adding sort

--- vueApp.js
+++ vueApp.js
@@ -3,7 +3,16 @@ const url = "/api/getdevices";
 const vm = new Vue({
         el: '#app',
         data: {
-          fields: ['deviceName', 'requestedDate', 'deviceId','status','approximateAmount','actualAmount'],
+          sortBy: 'status',
+          sortDesc: false,
+          fields: ['deviceName', 'requestedDate', 'deviceId','status','approximateAmount','actualAmount',
+          { key: 'deviceName', sortable: true },
+          { key: 'requestedDate', sortable: true },
+          { key: 'deviceId', sortable: true },
+          { key: 'status', sortable: false },
+          { key: 'approximateAmount', sortable: true },
+          { key: 'actualAmount', sortable: true }
+        ],
           items: []
         },
         mounted() {
